a39f5a4b7bd390cb3dede06d2dee07784c05fd46
NakulKait <nakulkait@gmail.com>
2025-04-14 09:24:23 +0530

    added database sql file

---
+++ db/db.sql
@@ -0,0 +1,29 @@
+create database hackathon;
+use hackathon;
+
+create table user(
+    id int auto_increment primary key,
+    full_name varchar(50),
+    email varchar(30),
+    password varchar(100),
+    phone_no char(10) ,
+    created_time  datetime default current_timestamp
+)
+
+create table blogs(
+    id int auto_increment primary key,
+    title varchar(20),
+    contents varchar(30),
+    created_time datetime default current_timestamp,
+        user_id int ,
+    category_id  int,
+    foreign key (user_id) references user(id),
+    foreign key (category_id) references categories(id)
+);
+
+
+create table categories(
+    id int primary key ,
+    title varchar(30),
+    description varchar(50)
+)
